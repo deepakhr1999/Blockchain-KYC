479f75f5c34e1d9c300ea0c6f031beeced5a18c8
Deepak H R <deepakhr1999@gmail.com>
2019-04-30 16:03:21 +0530

    UI changes to landing page

--- blockchainUI/index.php
+++ blockchainUI/index.php
@@ -42,15 +42,27 @@
         font-family: 'Lato', sans-serif;
       }
       section{
-        margin: 5px;
-        padding: 1em;
+        margin: 0;
+        padding: 3em 1em;
         display: flex; flex-wrap: wrap;
-        align-items: center;
-        justify-content: center;
+        justify-content: space-around;
+        transition: 0.6s ease-in-out;
       }
-      form, img{
+      section:hover{
+      	background-color: #eaeafa;
+      }
+      .inline{
         width: 30em;
+        align-self: center;
+        margin: auto 2em;
       }
+      .bg{
+      	width: 100%;
+      	object-fit: cover;
+      	height: 10em;
+      }
+
+
     </style>
   </head>
   <body>
@@ -69,58 +81,12 @@
           $_SESSION["message"] = "";
         }
       }
-      ?>  
-    <section>
-      <!-- Request a kyc for end users -->
-      <form class="m-3 p-3" method="POST" enctype="multipart/form-data"
-        action="<?php echo htmlspecialchars($upload);?>">
-        <legend>Request a KYC</legend><hr>
-        <div class="form-group">
-          <label>Name:</label>
-          <input type="text" class="form-control" placeholder="Full name" name="name">
-        </div>
+      ?> 
 
-        <div class="form-group">
-          <label>Date of birth:</label>
-          <input type="date" class="form-control" name="dob" id="date">
-        </div>
-
-        <div class="form-group">
-          <label>Phone</label>
-          <input type="text" class="form-control" placeholder="10 digit phone number" name="phone">
-        </div>
-
-        <div class="form-group">
-          <label>Aadhar number:</label>
-          <input type="text" class="form-control" placeholder="12 digits" name="aadhar">
-        </div>
-
-        <div class="form-group">
-          <label>Photo of document:</label>
-          <input type="file" name="file">
-        </div>
-
-
-        <?php 
-            $sql = "select distinct Bank from data.peers";
-            $result = call($sql);
-            if($result->num_rows>0){
-              echo '<div class="form-group">';
-              echo '<label>Bank:</label>';
-              echo '<select name="bank" class="custom-select" placeholder="Choose your bank">';
-              while($row = $result->fetch_assoc()){
-                if($row["Bank"] != "Bank_admin"){
-                  echo '<option value="'.$row["Bank"].'">'.$row["Bank"].'</option>';
-                }
-              }
-              echo '</select></div>';
-            }
-         ?>
-        <button class="btn btn-block btn-primary" name="form" value="kyc">Submit</button>
-      </form>
-
+	<section>
+      	<img class="inline" src="images/intro-img.svg">
       <!-- Login as a bank -->
-        <form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+        <form class="inline" method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
           <legend>Login as bank</legend><hr>
            <div class="form-group">
             <label>Name:</label>
@@ -139,15 +105,60 @@
             Login
           </button>
         </form>
-      </div>
     </section>
-      
+    <section>
+    	<img class="inline" src="images/about-extra-2.svg">
+    	<!-- Request a kyc for end users -->
+		<form class="inline" method="POST" enctype="multipart/form-data"
+		action="<?php echo htmlspecialchars($upload);?>">
+			<legend>Request a KYC</legend><hr>
+			<div class="form-group">
+			  <label>Name:</label>
+			  <input type="text" class="form-control" placeholder="Full name" name="name">
+			</div>
 
-    <script>
-      // const changename = (tag)=>{
-      //   document.getElementById('fileName').innerHTML = tag.value;
-      // }
-  </script> 
+			<div class="form-group">
+			  <label>Date of birth:</label>
+			  <input type="date" class="form-control" name="dob" id="date">
+			</div>
+
+			<div class="form-group">
+			  <label>Phone</label>
+			  <input type="text" class="form-control" placeholder="10 digit phone number" name="phone">
+			</div>
+
+			<div class="form-group">
+			  <label>Aadhar number:</label>
+			  <input type="text" class="form-control" placeholder="12 digits" name="aadhar">
+			</div>
+
+			<div class="form-group">
+			  <label>Photo of document:</label>
+			  <input type="file" name="file">
+			</div>
+
+
+			<?php 
+			    $sql = "select distinct Bank from data.peers";
+			    $result = call($sql);
+			    if($result->num_rows>0){
+			      echo '<div class="form-group">';
+			      echo '<label>Bank:</label>';
+			      echo '<select name="bank" class="custom-select" placeholder="Choose your bank">';
+			      while($row = $result->fetch_assoc()){
+			        if($row["Bank"] != "Bank_admin"){
+			          echo '<option value="'.$row["Bank"].'">'.$row["Bank"].'</option>';
+			        }
+			      }
+			      echo '</select></div>';
+			    }
+			 ?>
+			<button class="btn btn-block btn-primary" name="form" value="kyc">Submit</button>
+		</form>
+    </section>
+    <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #1c4fc3; height: 15em;">
+    	
+    </nav>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
